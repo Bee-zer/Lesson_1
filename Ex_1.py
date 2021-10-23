066c8149d5b7692ecff902fdd24d0cc9c51ed20d
Alexey <zoom-alex@yandex.ru>
2021-10-23 13:26:59 +0300

    Initial commit

---
+++ Ex_1.py
@@ -0,0 +1,9 @@
+a = 10
+b = 'OK'
+c = 444.3
+d = c != 444.2
+
+print('a =', a, '  type:', type(a))
+print('b =', b, '  type:', type(b))
+print('c =', c, '  type:', type(c))
+print('d =', d, '  type:', type(d))
